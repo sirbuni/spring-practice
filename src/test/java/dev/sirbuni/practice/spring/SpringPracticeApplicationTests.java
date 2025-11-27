c3135fdfa854264f9c78536821a2c85117b595b5
Sirbuni Dev <sirbuni.dev@gmail.com>
2025-11-27 10:06:51 +0300

    initial base skeleton for practice

---
+++ src/test/java/dev/sirbuni/practice/spring/SpringPracticeApplicationTests.java
@@ -0,0 +1,14 @@
+package dev.sirbuni.practice.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+        // testing context
+	}
+
+}
